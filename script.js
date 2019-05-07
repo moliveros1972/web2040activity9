31c3b6a7763b9b3c57cc5308c02f97d0f0d17855
moliveros1972 <44247022+moliveros1972@users.noreply.github.com>
2019-05-06 20:40:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function playvid() {
+	var homer = document.getElementById("homer");
+	homer.play();
+}
+
+function stopvid() {
+	var homer = document.getElementById("homer");
+	homer.pause();
+}
+
+
+
+
